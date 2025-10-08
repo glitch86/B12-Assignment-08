90c50511c9427753d1441a9eb8767e0a5a69d650
glitch86 <mdpolokahmed825@gmail.com>
2025-10-08 19:37:40 +0600

    added search funtionalities

--- src/Components/AppsPage/AllApps.jsx
+++ src/Components/AppsPage/AllApps.jsx
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import useData from "../../Hooks/useData";
 import Cards from "../Trending/Cards";
 
 const AllApps = () => {
   const { datas } = useData();
-  console.log(datas);
+  const [search, setSearch] = useState('');
+  const keyWord = search.trim().toLocaleLowerCase();
+
+    const searchRes = keyWord? datas.filter(data => data.title.toLocaleLowerCase().includes(keyWord)): datas
+
   return (
     <div>
       <div className="flex flex-col md:flex-row justify-between items-center">
-        <span className="text-2xl font-bold">({datas.length}) Apps found</span>
+        <span className="text-2xl font-bold">({searchRes.length}) Apps found</span>
 
-        <input
-          type="text"
-          className="input border border-violet-400 focus:outline-none focus:border-2"
-          placeholder="Search"
-        />
+        <label htmlFor="search" className="w-[300px]">
+          <input
+            id="search"
+            type="search"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className="input border border-violet-400 focus:outline-none focus:border-2"
+            placeholder="Search"
+          />
+        </label>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2 md:gap-3 lg:gap-4 my-8">
-        {datas.map((data) => (
+        {searchRes.map((data) => (
           <Cards key={data.id} data={data}></Cards>
         ))}
       </div>
